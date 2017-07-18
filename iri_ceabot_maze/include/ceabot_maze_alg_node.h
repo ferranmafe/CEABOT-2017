1f8a2023d48e5a0cdff08a7dbb646c33fe01677f
Ferran Martínez Felipe <fmartinez@iri.upc.edu>
2017-07-18 12:48:10 +0200

    Added first changes to maze trial

--- iri_ceabot_maze/include/ceabot_maze_alg_node.h
+++ iri_ceabot_maze/include/ceabot_maze_alg_node.h
@@ -1,5 +1,5 @@
 // Copyright (C) 2010-2011 Institut de Robotica i Informatica Industrial, CSIC-UPC.
-// Author 
+// Author
 // All rights reserved.
 //
 // This file is part of iri-ros-pkg
@@ -15,8 +15,8 @@
 //
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
-// 
-// IMPORTANT NOTE: This code has been generated through a script from the 
+//
+// IMPORTANT NOTE: This code has been generated through a script from the
 // iri_ros_scripts. Please do NOT delete any comments to guarantee the correctness
 // of the scripts. ROS topics can be easly add by using those scripts. Please
 // refer to the IRI wiki page for more information:
@@ -28,7 +28,19 @@
 #include <iri_base_algorithm/iri_base_algorithm.h>
 #include "ceabot_maze_alg.h"
 
+#include <humanoid_modules/walk_module.h>
+#include <humanoid_modules/head_tracking_module.h>
+#define PI 3.1415926535
+#define ERROR 0.130899693
+
 // [publisher subscriber headers]
+#include <sensor_msgs/Imu.h>
+#include <nav_msgs/Odometry.h>
+#include <sensor_msgs/JointState.h>
+#include <humanoid_common_msgs/tag_pose_array.h>
+#include <string>
+#include <set>
+#include <tf/tf.h>
 
 // [service client headers]
 
@@ -38,13 +50,42 @@
  * \brief IRI ROS Specific Algorithm Class
  *
  */
+
+ typedef enum {IDLE,
+               SCAN_MAZE,
+               CALCULATE_NEXT_MOVE,
+               MOVEMENT,
+               CHECK_GOAL} darwin_states;
+
 class CeabotMazeAlgNode : public algorithm_base::IriBaseAlgorithm<CeabotMazeAlgorithm>
 {
   private:
     // [publisher attributes]
 
     // [subscriber attributes]
+    ros::Subscriber imu_subscriber_;
+    void imu_callback(const sensor_msgs::Imu::ConstPtr& msg);
+    pthread_mutex_t imu_mutex_;
+    void imu_mutex_enter(void);
+    void imu_mutex_exit(void);
 
+    ros::Subscriber odom_subscriber_;
+    void odom_callback(const nav_msgs::Odometry::ConstPtr& msg);
+    pthread_mutex_t odom_mutex_;
+    void odom_mutex_enter(void);
+    void odom_mutex_exit(void);
+
+    ros::Subscriber joint_states_subscriber_;
+    void joint_states_callback(const sensor_msgs::JointState::ConstPtr& msg);
+    pthread_mutex_t joint_states_mutex_;
+    void joint_states_mutex_enter(void);
+    void joint_states_mutex_exit(void);
+
+    ros::Subscriber qr_pose_subscriber_;
+    void qr_pose_callback(const humanoid_common_msgs::tag_pose_array::ConstPtr& msg);
+    pthread_mutex_t qr_pose_mutex_;
+    void qr_pose_mutex_enter(void);
+    void qr_pose_mutex_exit(void);
     // [service attributes]
 
     // [client attributes]
@@ -59,11 +100,25 @@ class CeabotMazeAlgNode : public algorithm_base::IriBaseAlgorithm<CeabotMazeAlgo
     * This variable has all the driver parameters defined in the cfg config file.
     * Is updated everytime function config_update() is called.
     */
+    bool event_start;
+    darwin_states darwin_state;
+    bool half_maze_achieved;
+    bool search_started;
+    bool first_scan_goal_achieved;
+    bool second_scan_goal_achieved;
+    double pan_angle;
+    double current_pan_angle;
+    double tilt_angle;
+    double current_tilt_angle;
+
+    std::set<humanoid_common_msgs::tag_pose> qr_tags_detected;
     Config config_;
+    CWalkModule walk;
+    CHeadTrackingModule tracking_module;
   public:
    /**
     * \brief Constructor
-    * 
+    *
     * This constructor initializes specific class attributes and all ROS
     * communications variables to enable message exchange.
     */
@@ -71,7 +126,7 @@ class CeabotMazeAlgNode : public algorithm_base::IriBaseAlgorithm<CeabotMazeAlgo
 
    /**
     * \brief Destructor
-    * 
+    *
     * This destructor frees all necessary dynamic memory allocated within this
     * this class.
     */
@@ -82,11 +137,11 @@ class CeabotMazeAlgNode : public algorithm_base::IriBaseAlgorithm<CeabotMazeAlgo
     * \brief main node thread
     *
     * This is the main thread node function. Code written here will be executed
-    * in every node loop while the algorithm is on running state. Loop frequency 
+    * in every node loop while the algorithm is on running state. Loop frequency
     * can be tuned by modifying loop_rate attribute.
     *
     * Here data related to the process loop or to ROS topics (mainly data structs
-    * related to the MSG and SRV files) must be updated. ROS publisher objects 
+    * related to the MSG and SRV files) must be updated. ROS publisher objects
     * must publish their data in this process. ROS client servers may also
     * request data to the corresponding server topics.
     */
@@ -94,12 +149,12 @@ class CeabotMazeAlgNode : public algorithm_base::IriBaseAlgorithm<CeabotMazeAlgo
 
    /**
     * \brief dynamic reconfigure server callback
-    * 
+    *
     * This method is called whenever a new configuration is received through
-    * the dynamic reconfigure. The derivated generic algorithm class must 
+    * the dynamic reconfigure. The derivated generic algorithm class must
     * implement it.
     *
-    * \param config an object with new configuration from all algorithm 
+    * \param config an object with new configuration from all algorithm
     *               parameters defined in the config file.
     * \param level  integer referring the level in which the configuration
     *               has been changed.
@@ -109,13 +164,36 @@ class CeabotMazeAlgNode : public algorithm_base::IriBaseAlgorithm<CeabotMazeAlgo
    /**
     * \brief node add diagnostics
     *
-    * In this abstract function additional ROS diagnostics applied to the 
+    * In this abstract function additional ROS diagnostics applied to the
     * specific algorithms may be added.
     */
     void addNodeDiagnostics(void);
 
+    /**
+     * \brief state machine
+     *
+     * State Machine. After the start the idea is to iterate between the different states
+     * SEARCH_QR -> DECODE_QR -> MOVEMENT -> MOVEMENT_ERROR_COMPROBATION -> SEARCH_QR
+     */
+     void state_machine(void);
+
+     /**
+      * \brief init walk module
+      *
+      * Function which initialize all the walk parameters of Darwin from the .cfg file
+      */
+     void init_walk_module(void);
+
+     /**
+      * \brief init head tracking module
+      *
+      * Function which initialize all the head tracking parameters of Darwin from the .cfg file
+      */
+      void init_headt_module(void);
+
+      void calculate_next_move(void);
     // [diagnostic functions]
-    
+
     // [test functions]
 };
 
--- iri_ceabot_maze/src/ceabot_maze_alg_node.cpp
+++ iri_ceabot_maze/src/ceabot_maze_alg_node.cpp
@@ -1,41 +1,182 @@
 #include "ceabot_maze_alg_node.h"
 
 CeabotMazeAlgNode::CeabotMazeAlgNode(void) :
-  algorithm_base::IriBaseAlgorithm<CeabotMazeAlgorithm>()
+  algorithm_base::IriBaseAlgorithm<CeabotMazeAlgorithm>(),
+  walk("ceabot_maze_walk"),
+  tracking_module("ceabot_maze_track")
 {
   //init class attributes if necessary
   //this->loop_rate_ = 2;//in [Hz]
+  this->event_start = true;
+  this->darwin_state = SCAN_MAZE;
+  this->half_maze_achieved = false;
 
+  this->search_started=false;
+  this->first_scan_goal_achieved = false;
+  this->second_scan_goal_achieved = false;
   // [init publishers]
-  
+
   // [init subscribers]
-  
+  this->imu_subscriber_ = this->public_node_handle_.subscribe("imu", 1, &CeabotMazeAlgNode::imu_callback, this);
+  pthread_mutex_init(&this->imu_mutex_,NULL);
+
+  this->odom_subscriber_ = this->public_node_handle_.subscribe("odom", 1, &CeabotMazeAlgNode::odom_callback, this);
+  pthread_mutex_init(&this->odom_mutex_,NULL); //By the moment we're just going to use the bno055 sensor
+
+  this->joint_states_subscriber_ = this->public_node_handle_.subscribe("joint_states", 1, &CeabotMazeAlgNode::joint_states_callback, this);
+  pthread_mutex_init(&this->joint_states_mutex_,NULL);
+
+  this->qr_pose_subscriber_ = this->public_node_handle_.subscribe("qr_pose", 1, &CeabotMazeAlgNode::qr_pose_callback, this);
+  pthread_mutex_init(&this->qr_pose_mutex_,NULL);
   // [init services]
-  
+
   // [init clients]
-  
+
   // [init action servers]
-  
+
   // [init action clients]
 }
 
 CeabotMazeAlgNode::~CeabotMazeAlgNode(void)
 {
   // [free dynamic memory]
+  pthread_mutex_destroy(&this->imu_mutex_);
+  pthread_mutex_destroy(&this->odom_mutex_);
+  pthread_mutex_destroy(&this->joint_states_mutex_);
+  pthread_mutex_destroy(&this->qr_pose_mutex_);
 }
 
 void CeabotMazeAlgNode::mainNodeThread(void)
 {
   // [fill msg structures]
-  
+
   // [fill srv structure and make request to the server]
-  
+
   // [fill action structure and make request to the action server]
 
   // [publish messages]
+
+  if (!this->event_start) {
+    state_machine();
+  }
+
+  //Code for the start. It sets all the configuration from the .cfg to the modules.
+  //Then, waits for 5 sec (start condition - read CEABOT rules) and change the Darwin
+  //state to start searching for the first QR code
+  else {
+    //Initialization of Darwin parameters
+    init_walk_module();
+    init_headt_module();
+
+    //5 sec sleep. It's for the init of the event, the rules says that Darwin must
+    //wait this amount of time in seconds
+    ros::Duration(5.0).sleep();
+
+    //We must set down the start flag in terms of dont enter to this condition again
+    this->event_start = false;
+  }
 }
 
 /*  [subscriber callbacks] */
+void CeabotMazeAlgNode::imu_callback(const sensor_msgs::Imu::ConstPtr& msg) {
+  /*/ROS_INFO("CeabotMazeAlgNode::imu_callback: New Message Received");
+
+  //use appropiate mutex to shared variables if necessary
+  //this->alg_.lock();
+  //this->imu_mutex_enter();
+  double bnoaux = tf::getYaw(msg->orientation);
+  if (bnoaux < 0) bnoaux += 2*PI;
+  this->bno055_measurement = bnoaux; //We normalize the measurement...
+  //ROS_INFO("Darwin Ceabot Vision : The actual Yaw is : %f", this->bno055_measurement);
+  //std::cout << msg->data << std::endl;
+  //unlock previously blocked shared variables
+  //this->alg_.unlock();
+  //this->imu_mutex_exit();*/
+}
+
+void CeabotMazeAlgNode::imu_mutex_enter(void) {
+  pthread_mutex_lock(&this->imu_mutex_);
+}
+
+void CeabotMazeAlgNode::imu_mutex_exit(void) {
+  pthread_mutex_unlock(&this->imu_mutex_);
+}
+
+void CeabotMazeAlgNode::odom_callback(const nav_msgs::Odometry::ConstPtr& msg) {
+  //ROS_INFO("CeabotMazeAlgNode::odom_callback: New Message Received");
+
+  //use appropiate mutex to shared variables if necessary
+  //this->alg_.lock();
+  //this->odom_mutex_enter();
+
+  //std::cout << msg->data << std::endl;
+  //unlock previously blocked shared variables
+  //this->alg_.unlock();
+  //this->odom_mutex_exit();
+}
+
+void CeabotMazeAlgNode::odom_mutex_enter(void) {
+  pthread_mutex_lock(&this->odom_mutex_);
+}
+
+void CeabotMazeAlgNode::odom_mutex_exit(void) {
+  pthread_mutex_unlock(&this->odom_mutex_);
+}
+
+void CeabotMazeAlgNode::joint_states_callback(const sensor_msgs::JointState::ConstPtr& msg) {
+  unsigned int i;
+
+  //use appropiate mutex to shared variables if necessary
+  //this->alg_.lock();
+  this->joint_states_mutex_enter();
+
+  for (i = 0; i < msg->name.size() ; ++i){
+    if (msg->name[i]=="j_pan"){
+      this->current_pan_angle=msg->position[i];
+      if (this->darwin_state == SCAN_MAZE and this->search_started and this->current_pan_angle > PI/2) first_scan_goal_achieved = true;
+      else if (this->darwin_state == SCAN_MAZE and this->search_started and this->first_scan_goal_achieved and this->current_pan_angle < -PI/2) second_scan_goal_achieved = true;
+
+    }
+    else if (msg->name[i]=="j_tilt"){
+      this->current_tilt_angle=(msg->position[i]);
+    }
+  }
+  ROS_INFO("angle_pan: %f angle_tilt: %f",this->current_pan_angle,this->current_tilt_angle);
+  //unlock previously blocked shared variables
+  //this->alg_.unlock();
+  this->joint_states_mutex_exit();
+}
+
+void CeabotMazeAlgNode::joint_states_mutex_enter(void) {
+  pthread_mutex_lock(&this->joint_states_mutex_);
+}
+
+void CeabotMazeAlgNode::joint_states_mutex_exit(void) {
+  pthread_mutex_unlock(&this->joint_states_mutex_);
+}
+
+void CeabotMazeAlgNode::qr_pose_callback(const humanoid_common_msgs::tag_pose_array::ConstPtr& msg) {
+  //ROS_INFO("QrHeadTrackingAlgNode::qr_pose_callback: New Message Received");
+  //ROS_INFO("Detected %d QR tags",(int)msg->tags.size());
+  this->qr_pose_mutex_enter();
+  if(msg->tags.size()>0)
+  {
+    //std::cout << this->QR_identifier << std::endl;
+    //this->pan_angle=this->current_pan_angle+atan2(msg->tags[0].position.x,msg->tags[0].position.z);
+    //this->tilt_angle=this->current_tilt_angle+atan2(msg->tags[0].position.y,msg->tags[0].position.z);
+    //ROS_INFO("Next target pan angle: %f (%f,%f)",this->pan_angle,this->current_pan_angle,atan2(msg->tags[0].position.x,msg->tags[0].position.z));
+    //ROS_INFO("Next target tilt angle: %f (%f,%f)",this->tilt_angle,this->current_tilt_angle,atan2(msg->tags[0].position.y,msg->tags[0].position.z));
+  }
+  this->qr_pose_mutex_exit();
+}
+
+void CeabotMazeAlgNode::qr_pose_mutex_enter(void) {
+  pthread_mutex_lock(&this->qr_pose_mutex_);
+}
+
+void CeabotMazeAlgNode::qr_pose_mutex_exit(void) {
+  pthread_mutex_unlock(&this->qr_pose_mutex_);
+}
 
 /*  [service callbacks] */
 
@@ -43,19 +184,90 @@ void CeabotMazeAlgNode::mainNodeThread(void)
 
 /*  [action requests] */
 
-void CeabotMazeAlgNode::node_config_update(Config &config, uint32_t level)
-{
+void CeabotMazeAlgNode::node_config_update(Config &config, uint32_t level) {
   this->alg_.lock();
   this->config_=config;
   this->alg_.unlock();
 }
 
-void CeabotMazeAlgNode::addNodeDiagnostics(void)
-{
+void CeabotMazeAlgNode::addNodeDiagnostics(void) {}
+
+void CeabotMazeAlgNode::init_walk_module(void) {
+  //ROS_INFO("Darwin Ceabot Vision : Walk Parameters Initialization");
+  this->walk.set_x_offset(config_.X_OFFSET);
+  this->walk.set_y_offset(config_.Y_OFFSET);
+  this->walk.set_z_offset(config_.Z_OFFSET);
+  this->walk.set_roll_offset(config_.R_OFFSET);
+  this->walk.set_pitch_offset(config_.P_OFFSET);
+  this->walk.set_yaw_offset(config_.A_OFFSET);
+  this->walk.set_hip_pitch_offset(config_.HIP_PITCH_OFFSET);
+  this->walk.set_period(config_.PERIOD_TIME);
+  this->walk.set_ssp_dsp_ratio(config_.DSP_RATIO);
+  this->walk.set_fwd_bwd_ratio(config_.STEP_FB_RATIO);
+  this->walk.set_foot_height(config_.FOOT_HEIGHT);
+  this->walk.set_left_right_swing(config_.Y_SWAP_AMPLITUDE);
+  this->walk.set_top_down_swing(config_.Z_SWAP_AMPLITUDE);
+  this->walk.set_pelvis_offset(config_.PELVIS_OFFSET);
+  this->walk.set_arm_swing_gain(config_.ARM_SWING_GAIN);
+  this->walk.set_trans_speed(config_.MAX_VEL);
+  this->walk.set_rot_speed(config_.MAX_ROT_VEL);
+}
+
+void CeabotMazeAlgNode::init_headt_module(void) {
+  //ROS_INFO("Darwin Ceabot Vision : HeadTracking Parameters Initialization");
+  this->tracking_module.set_pan_range(config_.max_pan, config_.min_pan);
+  this->tracking_module.set_tilt_range(config_.max_tilt, config_.min_tilt);
+  this->tracking_module.set_pan_pid(config_.pan_p, config_.pan_i, config_.pan_d, config_.pan_i_clamp);
+  this->tracking_module.set_tilt_pid(config_.tilt_p, config_.tilt_i, config_.tilt_d, config_.tilt_i_clamp);
+}
+
+void CeabotMazeAlgNode::calculate_next_move(void) {
+
+}
+
+void CeabotMazeAlgNode::state_machine(void) {
+  switch(this->darwin_state) {
+    case IDLE: //0 ------------------------------------------------------------------------------------------------------------------------------------
+      ROS_INFO("Darwin Ceabot Vision : state IDLE");
+      break;
+
+    case SCAN_MAZE: //1 -------------------------------------------------------------------------------------------------------------------------------
+      ROS_INFO("Scanning Maze...");
+      if (!this->search_started) {
+        this->tracking_module.start_tracking(PI/2, 0.0);
+        this->search_started = true;
+      }
+      else if (!this->first_scan_goal_achieved) {
+        this->tracking_module.update_target(-PI/2, 0.0);
+      }
+      else if (!this->second_scan_goal_achieved) {
+        this->tracking_module.update_target(0.0, 0.0);
+      }
+      else {
+          darwin_state = CALCULATE_NEXT_MOVE;
+          search_started = false;
+          first_scan_goal_achieved = false;
+          second_scan_goal_achieved = false;
+      }
+
+      break;
+
+    case CALCULATE_NEXT_MOVE:
+      ROS_INFO("Calculating next move...");
+      calculate_next_move();
+      break;
+
+    case MOVEMENT: //2 ----------------------------------------------------------------------------------------------------------------------------------
+      ROS_INFO("Moving...");
+      break;
+
+    case CHECK_GOAL: //3 ------------------------------------------------------------------------------------------------------------------
+      ROS_INFO("Checking Movement Accuracy...");
+      break;
+  }
 }
 
 /* main function */
-int main(int argc,char *argv[])
-{
+int main(int argc,char *argv[]) {
   return algorithm_base::main<CeabotMazeAlgNode>(argc, argv, "ceabot_maze_alg_node");
 }
